8d6603e4aaf800c92e1795f572b3a5f0548432dd
twinklesu <twinklesu914@gmail.com>
2022-04-08 16:56:44 +0900

    connect all the buttons

--- popup.js
+++ popup.js
@@ -1,19 +1,22 @@
 $(function () {
   $("#start").on("click", function () {
     console.log("on click start");
-    ping();
+    ping("start");
   });
   $("#pre").on("click", function () {
     console.log("on click pre");
+    print("pre");
   });
   $("#next").on("click", function () {
     console.log("on click next");
+    ping("next");
   });
   $("#end").on("click", function () {
     console.log("on click end");
+    ping("end");
   });
 });
 
-function ping() {
-  chrome.runtime.sendMessage("ping", (response) => {});
+function ping(msg) {
+  chrome.runtime.sendMessage({ action: msg }, (response) => {});
 }
