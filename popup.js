429bf3a9a74386a406e33b4e0ccf21f50710a209
twinklesu <twinklesu914@gmail.com>
2022-05-09 22:07:02 +0900

    feature selection success

--- popup.js
+++ popup.js
@@ -1,26 +1,32 @@
+var featureName = "";
+chrome.storage.local.get(["featureName"], function (result) {
+  featureName = result.featureName;
+});
+
 $(function () {
+  var name;
+  if (featureName !== "") {
+    name = featureName;
+    console.log("(popup) 이전에 선택된 기능: ", featureName);
+    // 안골라도 선택된걸로 보이게,,,,,
+  }
+  $("#select1").on("click", function () {
+    console.log("on click select1");
+    name = "민원 신청 임시 저장";
+  });
+  $("#select2").on("click", function () {
+    console.log("on click select2");
+    name = "청년 임차 보증금 신청";
+  });
+  $("#select3").on("click", function () {
+    console.log("on click select3");
+    name = "평생 학습 수강 신청";
+  });
+
+  // 기능 선택 없이 다른 버튼 누르면 예외 처리 필요
   $("#start").on("click", function () {
     // 시작 버튼 눌렸을 때
     console.log("on click start");
-
-    // 여기서 feature 선택해서
-
-    // ping으로 넘겨주는 feature name 변수만 변경시켰음.
-    var name;
-    $("#select1").on("click", function () {
-      console.log("on click select1");
-      name = "민원 신청 임시 저장";
-    });
-    $("#select2").on("click", function () {
-      console.log("on click select2");
-      name = "청년 임차 보증금 신청";
-    });
-    $("#select3").on("click", function () {
-      console.log("on click select3");
-      name = "평생 학습 수강 신청";
-    });
-
-    var name = $(".select:checked").val(); // 라디오 버튼에서 체크된 값 가져오도록
     console.log(name);
     ping("start", name); // 여기에 feature 넘겨주면 됨!
   });
